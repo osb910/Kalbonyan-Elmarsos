426287b821a9ff68248a9b83c64b354f78cf4b8a
Omar Sherif <omarber79@gmail.com>
2022-04-02 10:03:48 +0200

    add fundamentals course

---
+++ LinkedIn-Learning/01_Programming-Foundation-Fundamentals/03_07_Challenge.py
@@ -0,0 +1,23 @@
+print("Challenge 1:")
+
+# Prints: "Challenge 1:"
+
+# A message for the user
+message = "This is going to be tricky ;)"
+Message = "Very tricky!"
+print(message) # show the message on the screen
+
+# Prints: "This is going to be tricky ;)"
+
+# Perform mathematical operations
+result = 2**3
+print("2**3 =", result)
+
+# Prints: "2**3 = 8"
+
+result = 5 - 3
+#print("5 - 3 =", result)
+
+print("Challenge complete!")
+
+# Prints: "Challenge complete"
---
+++ LinkedIn-Learning/01_Programming-Foundation-Fundamentals/04_05_Challenge.py
@@ -0,0 +1,11 @@
+# Guessing Game
+
+my_favorite_food = 'Potatoes'
+user_guess = input("What's my favorite food?")
+
+if user_guess == my_favorite_food:
+   print("Yep! So amazing!")
+else:
+   print("Yuck! That's not it")
+
+print("Thanks for playing!")
