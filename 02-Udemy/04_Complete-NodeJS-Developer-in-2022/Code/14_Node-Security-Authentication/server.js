e606be0a341829ec8c3e660ff423411cea5b4ea6
Omar Shareef Abdul-Bar <omarshdev@gmail.com>
2022-08-09 21:55:47 +0200

    Add Security starter code

---
+++ 02-Udemy/04_Complete-NodeJS-Developer-in-2022/Code/14_Node-Security-Authentication/server.js
@@ -0,0 +1,39 @@
+// const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+// const helmet = require('helmet');
+// const passport = require('passport');
+// const {Strategy} = require('passport-google-oauth20');
+// const cookieSession = require('cookie-session');
+// const {verify} = require('crypto');
+
+// require('dotenv').config();
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret value is 42!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}...`);
+// });
+
+https
+  .createServer(
+    {
+      // key: fs.readFileSync('key.pem'),
+      // cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
