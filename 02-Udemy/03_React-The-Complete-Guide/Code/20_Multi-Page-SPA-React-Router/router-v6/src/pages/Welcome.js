1195af39eba9a6ac3df1c1148d28e45d2ca59a11
Omar Shareef Abdul-Bar <omarshdev@gmail.com>
2022-08-28 21:55:19 +0200

    router v6

---
+++ 02-Udemy/03_React-The-Complete-Guide/Code/20_Multi-Page-SPA-React-Router/router-v6/src/pages/Welcome.js
@@ -0,0 +1,12 @@
+import {Outlet} from 'react-router-dom';
+
+const Welcome = () => {
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Outlet />
+    </section>
+  );
+};
+
+export default Welcome;
